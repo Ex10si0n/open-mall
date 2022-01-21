f1506745ef5c6825b3bcb09c537f9e10bcfb3077
Ex10si0n <yzb.ex10si0n@icloud.com>
2022-01-21 17:14:24 +0800

    update: add requirements TODO, current done: add product

--- isi-api/db/database.py
+++ isi-api/db/database.py
@@ -9,14 +9,6 @@ connection = pymysql.connect(host=settings.db_host,
                              database=settings.database,
                              cursorclass=pymysql.cursors.DictCursor)
 
-def test():
-    with connection:
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM `account` WHERE `email`=%s"
-            cursor.execute(sql, ('p1908326@ipm.edu.mo',))
-            result = cursor.fetchall()[0]
-            print(result)
-
 def create_account(accName: str, email: str, password: str, accType: str):
     """ Create a new account
 
@@ -29,24 +21,24 @@ def create_account(accName: str, email: str, password: str, accType: str):
     Returns:
         dict: status, uuid
     """    
-    ret = {'status': '', 'uuid': ''}
+    playload = {'status': '', 'uuid': ''}
     with connection:
         with connection.cursor() as cursor:
             sql = "SELECT * FROM `account` WHERE `email`=%s"
             cursor.execute(sql, (email,))
             result = cursor.fetchall()
             if (len(result) != 0):
-                ret['status'] = 'email_exists'
-                return ret
+                playload['status'] = 'email_exists'
+                return playload
             sql = "INSERT INTO `account` (`ACCID`, `ACCNAME`, `HASHEDPASSWORD`, `EMAIL`, `ACCTYPE`) VALUES (%s, %s, %s, %s, %s)"
             accId = str(uuid.uuid4())
             cursor.execute(sql, (accId, accName, password_validator.hash(password, accId), email, accType))
             connection.commit()
-            ret['status'] = 'success'
-            ret['uuid'] = accId
-            return ret
-    ret['status'] = 'error'
-    return ret
+            playload['status'] = 'success'
+            playload['uuid'] = accId
+            return playload
+    playload['status'] = 'error'
+    return playload
 
 def login_check(email: str, password: str):
     """ Login check
@@ -58,28 +50,67 @@ def login_check(email: str, password: str):
     Returns:
         dict: status, uuid, type
     """    
-    ret = {'status': '', 'uuid': '', 'type': ''}
+    playload = {'status': '', 'uuid': '', 'type': ''}
     with connection:
         with connection.cursor() as cursor:
             sql = "SELECT * FROM `account` WHERE `email`=%s"
             cursor.execute(sql, (email,))
             result = cursor.fetchone()
             if (result is None):
-                ret['status'] = 'account_not_registered'
-                return ret
+                playload['status'] = 'account_not_registered'
+                return playload
             given_hashed_password = password_validator.hash(password, result['ACCID'])
             if given_hashed_password == result['HASHEDPASSWORD']:
-                ret['status'] = 'success'
-                ret['uuid'] = result['ACCID']
-                ret['type'] = result['ACCTYPE']
-                return ret
+                playload['status'] = 'success'
+                playload['uuid'] = result['ACCID']
+                playload['type'] = result['ACCTYPE']
+                return playload
             else:
-                ret['status'] = 'invalid_password'
-                return ret
-    ret['status'] = 'error'
-    return ret
+                playload['status'] = 'invalid_password'
+                return playload
+    playload['status'] = 'error'
+    return playload
+
+# @TODO: delete a user by email permanently
+
+# @TODO: update user password
+
+# @TODO: create user's address
     
-# @TODO: create product
+# @TODO: get user's address by addressID
+
+# @TODO: update user's address
+
+# @TODO: delete user's address
+
+def create_product(pName: str, brand: str, price: float, pDesc: str, thumbnail: str, pic: str, category: str):
+    """ Create a new product
+
+    Args:
+        pName (str): product name
+        brand (str): product brand
+        price (float): product price  @TODO: currency calculation
+        pDesc (str): product description
+        thumbnail (str): thumbnail image
+        pic (str): product detail image
+        category (str): product category
+
+    Returns:
+        dict: status, pid
+    """    
+    playload = {'status': '', 'pid': ''}
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO `product` (`PID`, `PNAME`, `BRAND`, `PRICE`, `PDESC`, `THUMBNAIL`, `PIC`, `CATALOG`)  VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            pid = str(uuid.uuid4())
+            cursor.execute(sql, (pid, pName, brand, price, pDesc, thumbnail, pic, category))
+            connection.commit()
+            playload['status'] = 'success'
+            playload['pid'] = pid
+            return playload
+    playload['status'] = 'error'
+    return playload
+
 
 # @TODO: get products by name
 
@@ -87,8 +118,20 @@ def login_check(email: str, password: str):
 
 # @TODO: delete product
 
+# @TODO: add product to shopping cart list
+
+# @TODO: delete certain product from shopping cart list
+
+# @TODO: clear shopping cart list without purchasing
+
+# @TODO: purchase all products in shopping cart list and clear shopping cart
+# should create a 'purchase receipt' by the following methods
+
+# @TODO: generate a 'puechase recepit' by method purchase all products in shopping cart list
+
 if __name__ == '__main__':
     res = None
     # res = create_account("Steve", 'p1908326@ipm.edu.mo', password='somepassword', accType='admin')
-    res = login_check("p1908326@ipm.edu.mo", password='somepassword')
+    # res = login_check("p1908326@ipm.edu.mo", password='somepassword')
+    res = create_product("iPhone 13 pro", "Apple", "10899", "Phone", "img/iphone.png", "img/iphone-2.png", "Smart Phone")
     print(res)
