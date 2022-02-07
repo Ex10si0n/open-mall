46250d06797a870b18bbd40c1b87fc99668db20d
Ex10si0n <yzb.ex10si0n@icloud.com>
2022-02-07 12:05:18 +0800

    update: order page

--- isi-web/src/views/Order.vue
+++ isi-web/src/views/Order.vue
@@ -23,12 +23,45 @@ const userEmail = computed(() => {
         </div>
       </h2>
       <div>
-        <button
-          type="submit"
-          class="group relative w-full flex justify-center py-3 px-6 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          My Orders
-        </button>
+        <ul role="list" class="-my-6 divide-y divide-gray-200">
+          <li class="py-6 flex">
+            <div
+              class="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden"
+            >
+              <img
+                src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg"
+                alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
+                class="w-full h-full object-center object-cover"
+              />
+            </div>
+
+            <div class="ml-4 flex-1 flex flex-col">
+              <div>
+                <div class="flex justify-between text-base font-medium text-gray-900">
+                  <h3>
+                    <a href="#"> Throwback Hip Bag </a>
+                  </h3>
+                  <p class="ml-4">$90.00</p>
+                </div>
+                <p class="mt-1 text-sm text-gray-500">
+                  Salmon | Qty 1 | Status: Delivered
+                </p>
+              </div>
+              <div class="flex-1 flex items-end justify-between text-sm">
+                <p class="text-gray-500">SF21376126328832651</p>
+
+                <div class="flex">
+                  <button
+                    type="button"
+                    class="font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Detailed
+                  </button>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
