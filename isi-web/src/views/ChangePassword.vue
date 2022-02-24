2b5935834744d35f714bfdd590f252a4c6011648
ex10si0n <yzb.ex10si0n@icloud.com>
2022-02-24 22:29:37 +0800

    opt: Change Password View

--- isi-web/src/views/ChangePassword.vue
+++ isi-web/src/views/ChangePassword.vue
@@ -51,46 +51,72 @@ const checkPassword = () =>{
 </script>
 
 <template>
-    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div class="max-w-md w-full space-y-8">
-            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Change Password
-            </h2>
+  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <img
+            alt="Workflow"
+            class="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+        />
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Change Password
+        </h2>
+      </div>
+
+      <div class="rounded-md shadow-sm -space-y-px">
+        <div>
+          <label class="sr-only" for="email-address">Email address</label>
+          <input
+              id="oldPassword"
+              v-model="oldPassword"
+              autocomplete="email"
+              class="appearance-none rounded-none relative block w-full px-3 py-4 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              name="email"
+              placeholder="Old Password"
+              required
+              type="password"
+          />
         </div>
         <div>
-        <div class="rounded-md shadow-sm -space-y-px">
-            <div>
-                <label class="sr-only" for="password">Old Password</label>
-                <input
-                id="oldPassword"
-                v-model="oldPassword"
-                class="appearance-none rounded-none relative block w-full px-3 py-4 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                name="oldPassword"
-                placeholder="Old Password"
-                required
-                type="password"
-                />
-            </div>
-            <div>
-                <label class="sr-only" for="password">New Password</label>
-                <input
-                    id="newPassword"
-                    v-model="newPassword"
-                    class="appearance-none rounded-none relative block w-full px-3 py-4 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    name="newPassword"
-                    placeholder="New Password"
-                    required
-                    type="password"
-                    />
-            </div>
-                <button
-                class="group relative w-full flex justify-center py-3 px-6 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                type="submit"
-                @click="changePassword"
-                >
-                Change Password
-                </button>
+          <label class="sr-only" for="password">Password</label>
+          <input
+              id="newPassword"
+              v-model="newPassword"
+              class="appearance-none rounded-none relative block w-full px-3 py-4 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              name="password"
+              placeholder="New Password"
+              required
+              type="password"
+          />
         </div>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="flex items-center">
+        </div>
+
+        <div class="text-sm">
+          <a class="font-medium text-indigo-600 hover:text-indigo-500" href="#">
+            Forgot your password?
+          </a>
+        </div>
+
+      </div>
+
+      <div>
+        <button
+            class="group relative w-full flex justify-center py-3 px-6 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            type="submit"
+            @click="changePassword"
+        >
+          Submit Change
+        </button>
+      </div>
+    </div>
+  </div>
+    <div class="min-h-full flex items-center justify-center">
+        <div class="rounded-md shadow-sm">
         </div>
     </div>
 </template>
